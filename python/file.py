f80df8434658133f64497ab5e9d7f34d3b0aca1d
vinshuljain <shalinijain1406@gmail.com>
2022-05-20 05:29:57 -0700

    comment

--- python/file.py
+++ python/file.py
@@ -1,6 +1,6 @@
 #use open function to read the content of a file
 
-f=open('sample.txt','r') #open is a in built function
+f=open("file.txt",'r') #open is a in built function
 # f=open('sample.txt')# by default it read the file
 # data=f.read()   #read is also a in built funtion
 # data=f.read(5) # it will read first five character
@@ -8,12 +8,14 @@ f=open('sample.txt','r') #open is a in built function
 # f.close()#remamber we should always close the file
 
 #other function to read the file
-
+print(f.tell())#tell the position of file pointer
 data=f.readline() #read first  line
 print(data) 
+print(f.tell())
 data=f.readline() #read second line
 print(data)
 data=f.readline() #so on....
+f.seek(0)  #it will reset line
 print(data)
 data=f.readline()
 print(data)
